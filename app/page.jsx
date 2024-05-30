c2dba96c0aac7af2db95de07b534ffb9e3b7353d
paduck-96 <peterrabitt1234@naver.com>
2024-05-30 11:10:01 +0900

    Feat: HomePage

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+        <>
+            <h1>Paduck Simple Blog</h1>
+            <p>
+                개인 작성 블로그 개발
+            </p>
+        </>
+    );
+}
