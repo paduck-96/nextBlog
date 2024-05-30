44707659d957ecc641d46fae2c53d0b7edf77c72
paduck-96 <peterrabitt1234@naver.com>
2024-05-30 15:51:24 +0900

    Feat: Recap Page

---
+++ app/recap/page.tsx
@@ -0,0 +1,10 @@
+export default function RecapPage() {
+    return (
+        <>
+            <h1>Recaps</h1>
+            <p>
+                All of my study for recaps.
+            </p>
+        </>
+    );
+}
