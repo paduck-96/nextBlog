2948d596efa5caa4f7bbc7b71cb8996e2b597243
paduck-96 <peterrabitt1234@naver.com>
2024-05-30 17:21:47 +0900

    Feat: layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from "react";
+import Link from "next/link";
 
 interface layoutProps {
     children: ReactNode
@@ -9,13 +10,22 @@ export default function RootLayout({ children }: layoutProps) {
         <html lang="en">
             <body>
             <header>
-                [header]
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/recap" prefetch={false}>Reviews</Link>
+                        </li>
+                    </ul>
+                </nav>
             </header>
             <main>
                 {children}
             </main>
             <footer>
-                [footer]
+                {/*해당 부분은 요구사항에 맞춰 작성될 예정*/}
             </footer>
             </body>
         </html>
