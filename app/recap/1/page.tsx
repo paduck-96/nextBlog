2789df00608717e81af8367bc88521647c59654c
paduck-96 <peterrabitt1234@naver.com>
2024-05-31 15:11:48 +0900

    Feat: page for headings zz

---
+++ app/recap/1/page.tsx
@@ -0,0 +1,15 @@
+import Heading from "@/components/Heading";
+
+export default function One() {
+    return (
+        <>
+            <Heading>Next.js Review</Heading>
+            <img src="" alt=""
+                 width="640" height="360" className="mb-2 rounded"
+            />
+            <p>
+                This will be the recap for some topic.
+            </p>
+        </>
+    );
+}
