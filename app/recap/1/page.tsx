a3ae858ab3630a1dbdc6a1024b5767f4dcb03ea5
paduck-96 <peterrabitt1234@naver.com>
2024-05-31 16:00:31 +0900

    Feat: static file read

--- app/recap/1/page.tsx
+++ app/recap/1/page.tsx
@@ -1,6 +1,8 @@
 import Heading from "@/components/Heading";
+import {readFile} from "node:fs/promises";
 
-export default function One() {
+export default async function One() {
+    const text = await readFile('./content/recap/1.md', 'utf8')
     return (
         <>
             <Heading>Next.js Review</Heading>
@@ -8,7 +10,7 @@ export default function One() {
                  width="640" height="360" className="mb-2 rounded"
             />
             <p>
-                This will be the recap for some topic.
+                {text}
             </p>
         </>
     );
