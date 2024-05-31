5848c440a09f897a675f00a597a2c6ea28a58db9
paduck-96 <peterrabitt1234@naver.com>
2024-05-31 17:24:39 +0900

    Feat: read .md and render

--- app/recap/1/page.tsx
+++ app/recap/1/page.tsx
@@ -1,17 +1,19 @@
 import Heading from "@/components/Heading";
 import {readFile} from "node:fs/promises";
+import {marked} from "marked";
 
 export default async function One() {
     const text = await readFile('./content/recap/1.md', 'utf8')
+    const html = marked(text)
     return (
         <>
             <Heading>Next.js Review</Heading>
             <img src="" alt=""
                  width="640" height="360" className="mb-2 rounded"
             />
-            <p>
-                {text}
-            </p>
+            <article dangerouslySetInnerHTML={{__html: html}}
+                     className="max-w-screen-sm prose prose-slate"
+            />
         </>
     );
 }
