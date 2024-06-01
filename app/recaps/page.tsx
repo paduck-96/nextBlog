ca23b107ec4cdc71d40c6dafb37888a6046f6640
paduck-96 <peterrabitt1234@naver.com>
2024-06-01 17:18:49 +0900

    Update: array render

--- app/recaps/page.tsx
+++ app/recaps/page.tsx
@@ -8,14 +8,20 @@ export default async function RecapPages() {
     return (
         <>
             <Heading>Recaps</Heading>
-            <ul className="flex flex-col gap-3">
-                <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-                    <Link href="/recaps/next_static">
-                        <h2 className="py-1 text-center font-hiMelody font-semibold">
-                            Next.js
-                        </h2>
-                    </Link>
-                </li>
+            <ul className="flex flex-row flex-wrap gap-3">
+                {recaps.map((recap) => (
+                    <li key={recap.slug}
+                        className="bg-white border rounded shadow w-80 hover:shadow-xl">
+                        <Link href={`/recaps/${recap.slug}`}>
+                            <img src={recap.image} alt=""
+                                 width="320" height="180" className="rounded-t"
+                            />
+                            <h2 className="font-orbitron font-semibold py-1 text-center">
+                                {recap.title}
+                            </h2>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </>
     );
