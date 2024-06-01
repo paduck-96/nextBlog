731013ea8e8c3d6b6344d3fee91fdcc2aba6d9c4
paduck-96 <peterrabitt1234@naver.com>
2024-06-01 17:09:56 +0900

    Feat: recaps in RecapPage

--- app/recaps/page.tsx
+++ app/recaps/page.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import {getRecaps} from "@/lib/recaps";
 
-export default function RecapPage() {
+export default async function RecapPages() {
+    const recaps = await getRecaps();
+
     return (
         <>
             <Heading>Recaps</Heading>
             <ul className="flex flex-col gap-3">
                 <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-                    <Link href="/recap/1">
+                    <Link href="/recaps/next_static">
                         <h2 className="py-1 text-center font-hiMelody font-semibold">
                             Next.js
                         </h2>
