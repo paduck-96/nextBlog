895829a09f9a85617fddcfe84dea57439c40a2fc
paduck-96 <peterrabitt1234@naver.com>
2024-06-01 16:59:27 +0900

    Update: data format

--- api/countVisitor.tsx
+++ api/countVisitor.tsx
@@ -2,6 +2,6 @@ let visitCount: number = 0;
 
 export default function handler(req: any, res: { status: (arg: number) => { json: { (arg: { visitCount: number; date: string; }): void}}}) {
     visitCount += 1;
-    const today = new Date().toISOString().split('T')[0];
+    const today = new Date().toLocaleDateString()
     res.status(200).json({ visitCount, date: today})
 }
